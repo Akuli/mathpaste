c8b7f4643ca9c8aa987f919759766206d0f0f46d
Akuli <akuviljanen17@gmail.com>
2018-10-21 12:54:51 +0300

    oops, i didn't realize it created global variables

--- js/firebase.js
+++ js/firebase.js
@@ -24,24 +24,26 @@ these are the rules i use:
 TODO: delete old maths regularly?
 */
 
-// 'await post("asd")' puts "asd" to firebase and returns an ID string
-async function post(math) {
-    // ref represents the object that represents the math in firebase
-    const ref = await firebase.database().ref("maths").push();
-    ref.set({ content: math, timestamp: (new Date()).valueOf() });
-    return ref.key;
-}
+(function() {
+    // 'await post("asd")' puts "asd" to firebase and returns an ID string
+    async function post(math) {
+        // ref represents the object that represents the math in firebase
+        const ref = await firebase.database().ref("maths").push();
+        ref.set({ content: math, timestamp: (new Date()).valueOf() });
+        return ref.key;
+    }
 
-// 'await get(an ID from post)' returns a math string
-// TODO: handle errors
-async function get(pasteId) {
-    const value = await firebase.database().ref(`maths/${pasteId}`).once("value");
-    console.log(value.val());
-    return value.val().content;
-}
+    // 'await get(an ID from post)' returns a math string
+    // TODO: handle errors
+    async function get(pasteId) {
+        const value = await firebase.database().ref(`maths/${pasteId}`).once("value");
+        console.log(value.val());
+        return value.val().content;
+    }
 
-// for require.js
-define({
-    post: post,
-    get: get
-});
+    // for require.js
+    define({
+        post: post,
+        get: get
+    });
+}());
