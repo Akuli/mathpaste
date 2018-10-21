6451f7ec0a322591e782984fc0208fc90584b178
Akuli <akuviljanen17@gmail.com>
2018-10-21 13:52:05 +0300

    add drawing code

---
+++ js/draw.js
@@ -0,0 +1,95 @@
+/*
+i experimented with different ways to convert the image data to a string:
+
+1) coordinates of end points of drawn lines joined with separator characters
+2) the full image data as one character for each pixel
+
+strings from 2 are of course about 1000 times longer than strings from 1, and
+that was still true after compressing with lzstring, with almost nothing drawn
+on the canvas and with lots of stuff drawn
+*/
+
+// https://github.com/requirejs/example-multipage/blob/master/www/js/app/controller/c1.js
+define(["./lz-string.min.js"], function(LZString) {
+
+    // contains arrays of lines
+    // each line is an array of 1 or more points that are connected to form the line
+    // each point is an [x,y] array
+    const drawnLines = [];
+
+    // undo is done with a window event listener because it works, but that
+    // must do nothing when the drawing area is not showing
+    let undoEnabled = false;
+
+    const canvas = document.getElementById('draw-canvas');
+    const context = canvas.getContext('2d');
+    let currentlyDrawingALine = false;
+
+    const xyFromEvent = mouseEvent => {
+        // there are two properties that give correct values, one is
+        // "experimental" and the other is "non-standard", so i chose the
+        // experimental property
+        // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/offsetX
+        // https://developer.mozilla.org/en-US/docs/Web/API/UIEvent/layerX
+        return [ event.offsetX, event.offsetY ];
+    };
+
+    canvas.addEventListener('mousedown', event => {
+        currentlyDrawingALine = true;
+        context.beginPath();
+        drawnLines.push([ xyFromEvent(event) ]);
+    });
+
+    canvas.addEventListener('mousemove', event => {
+        if (currentlyDrawingALine) {
+            context.beginPath();
+            const line = drawnLines[drawnLines.length - 1];
+            const xyNew = xyFromEvent(event);
+            context.moveTo(...(line[line.length - 1]));
+            context.lineTo(...xyNew);
+            context.stroke();
+            line.push(xyNew);
+        }
+    });
+
+    // document because mouse up outside canvas must also stop drawing
+    document.addEventListener('mouseup', () => {
+        currentlyDrawingALine = false;
+    });
+
+    const redrawEverything = theContext => {
+        theContext.clearRect(0, 0, theContext.canvas.width, theContext.canvas.height);
+        theContext.beginPath();
+        for (let line of drawnLines) {
+            theContext.moveTo(...( line[0] ));
+            for (let xy of line.slice(1)) {
+                theContext.lineTo(...xy);
+            }
+        }
+        theContext.stroke();
+    };
+
+    // https://stackoverflow.com/a/16006607
+    // adding the event listener to the canvas doesn't work
+    document.addEventListener('keydown', event => {
+        if (event.key == 'z' && event.ctrlKey && !currentlyDrawingALine && undoEnabled) {
+            drawnLines.pop();
+            redrawEverything(context);
+        }
+    });
+
+    return {
+        setUndoEnabled(value) {
+            undoEnabled = value;
+        },
+        getImageString() {
+            // each line is like x1,y1;x2,y2;... and the lines are separated by |
+            const lineString = drawnLines.map(line => line.map(xy => xy.join(',')).join(';')).join('|');
+            return LZString.compress(lineString);
+        },
+        setImageString(compressed) {
+            const lineString = LZString.decompress(lineString);
+            console.log(lineString);
+        }
+    };
+});
--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@
     // under RequireJs, but it still just defines its stuff under
     // `window.MathJax`, not with a `requirejs.define`.  ¯\_(ツ)_/¯
     const MATHJAX_URL = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=AM_HTMLorMML&delayStartupUntil=configured";
-    require([MATHJAX_URL, "./js/lz-string.min.js", "./js/firebase.js", "ace/ace"], function(_, LZString, firebase, ace) {
+    require([MATHJAX_URL, "./js/lz-string.min.js", "./js/firebase.js", "./js/draw.js", "ace/ace"], function(_, LZString, firebase, draw, ace) {
         const $renderedLines = document.getElementById(RENDERED_LINES_ID);
         let lineElements = [];
 
