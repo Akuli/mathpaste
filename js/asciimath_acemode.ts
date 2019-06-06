7e543df1946d58e7c75982a63a84c370bfbeaf69
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-06-07 00:56:11 +0200

    refactor: generalize embedding rules

--- js/asciimath_acemode.ts
+++ js/asciimath_acemode.ts
@@ -45,11 +45,17 @@ const literateModeState: HighlightState = [
   {
     token: "comment.block",
     regex: /^> /,
-    next: "asciimath",
+    next: "asciimath-start",
   }
 ];
 
-function defineMode(name: string, highlightRules: HighlightRules) {
+type Embed = {
+  moduleName: string,
+  highlightClassName: string,
+  extraState: HighlightState,
+};
+
+function defineMode(name: string, highlightRules: HighlightRules, embeds: Embed[] = []) {
   const define = (ace as any).define;
 
   define(
@@ -65,14 +71,28 @@ function defineMode(name: string, highlightRules: HighlightRules) {
     },
   );
 
+  const highlightRulesDependencies = ["require", "exports", "ace/lib/oop", "ace/mode/text_highlight_rules"];
+
+  embeds.forEach(embed => highlightRulesDependencies.push(`ace/mode/${embed.moduleName}_highlight_rules`));
+
   define(
     `ace/mode/${name}_highlight_rules`,
-    ["require", "exports", "ace/lib/oop", "ace/mode/text_highlight_rules"],
+    highlightRulesDependencies,
     (acequire: any, exports: any): void => {
       const TextHighlightRules = acequire("ace/mode/text_highlight_rules").TextHighlightRules;
 
       exports.HighlightRules = class extends TextHighlightRules {
         $rules: HighlightRules = highlightRules;
+
+        constructor() {
+          super();
+
+          embeds.forEach(embed => this.embedRules(
+            acequire(`ace/mode/${embed.moduleName}_highlight_rules`)[embed.highlightClassName],
+            `${embed.moduleName}-`,
+            embed.extraState
+          ));
+        }
       };
     }
   );
@@ -81,19 +101,24 @@ function defineMode(name: string, highlightRules: HighlightRules) {
 defineMode("asciimath",
   {
     start: asciimathState,
-  }
+  },
 );
 
 defineMode("literate_asciimath",
   {
     start: literateModeState,
-    asciimath: [
-      ...asciimathState,
+  },
+  [
+    {
+    moduleName: "asciimath",
+    highlightClassName: "HighlightRules",
+    extraState: [
       {
         token: "",
         regex: /$/,
         next: "start",
       },
-    ],
-  }
+    ]
+    },
+  ]
 );
