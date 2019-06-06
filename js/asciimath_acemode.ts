1fa25e89f2af99167688efddbfd2ef4bbc35e9d1
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-06-06 23:23:57 +0200

    refactor: prepare asciimath_acemode.ts for literate mode

--- js/asciimath_acemode.ts
+++ js/asciimath_acemode.ts
@@ -2,66 +2,76 @@
 
 import * as ace from "brace";
 
-const define = (ace as any).define;
+// Ace uses a state machine for highlighting. The next three type definitions
+// are a bit of arcane magic to make sure that we can only put states that
+// actually exist in "next" and that we define every state we nominate.
 
-type Acequire = (arg0: string) => any;
-type Exports = any;
+type HighlightStateTransition<S extends "start" | string = "start"> = {
+  token: string;
+  regex: RegExp,
+  next?: S,
+};
 
-define(
-  "ace/mode/asciimath",
-  ["require", "exports", "ace/mode/text", "ace/mode/asciimath_highlight_rules"],
-  (acequire: Acequire, exports: Exports) => {
-    const TextMode = acequire("ace/mode/text").Mode;
-    const AsciiMathHighlightRules = acequire("ace/mode/asciimath_highlight_rules").AsciiMathHighlightRules;
+type HighlightState<S extends "start" | string = "start"> = HighlightStateTransition<S>[];
 
-    class Mode extends TextMode {
-      HighlightRules: typeof AsciiMathHighlightRules = AsciiMathHighlightRules;
-    }
+type HighlightRules<KS extends "start" | string = "start"> = { [K in KS]: HighlightState<KS> }
 
-    exports.Mode = Mode;
+const asciimathState: HighlightState = [
+  {
+    token: "keyword.operator",
+    regex: /color\(.*?\)/,
   },
-);
+  {
+    token: "variable.other",
+    regex: /varepsilon|vartheta|epsilon|upsilon|lambda|Lambda|varphi|alpha|gamma|Gamma|delta|Delta|theta|Theta|kappa|sigma|Sigma|omega|Omega|beta|zeta|iota|eta|rho|tau|phi|Phi|chi|psi|Psi|mu|nu|xi|Xi|pi|Pi/,
+  },
+  {
+    token: "keyword.operator",
+    regex: /\\\\|backslashsetminus|twoheadrightarrowtail|twoheadrightarrow|frac\{2\}\{3\}|\/_\\|triangle|rightarrowtail|leftrightarrow|Leftrightarrow|\.\.\.\||rightarrow|Rightarrow|underbrace|root|therefore|ldots|cdots|downarrow|leftarrow|Leftarrow|underline|overbrace|\|\>\<\||bidwedge|emptyset|\|\\\ \||\|quad\||lceiling|rceiling|subseteq|supseteq|\>\-\>\>|overline|underset|partial|because|diamond|implies|uparrow|overset|\*\*\*|\|\>\<|ltimes|\>\<\||rtimes|bowtie|otimes|\^\^\^|bigvee|bigcap|bigcup|square|lfloor|rfloor|\-\<\=|preceq|\>\-\=|succeq|subset|supset|approx|propto|\<\=\>|forall|exists|\|\-\-|\|\=\=|models|langle|rangle|\>\-\>|\-\>\>|\|\-\>|mapsto|ubrace|obrace|cancel|arcsin|arccos|arctan|times|oplus|wedge|nabla|infty|aleph|vdots|ddots|angle|frown|notin|equiv|vdash|floor|color|cdot|\*\*|star|\/\/|\-\:|circ|odot|prod|\^\^|2\/3|2\^3|sqrt|oint|grad|\+\-|\:\.|\:\'|\|__|__\||\|\~|\~\||\!\=|\<\=|\>\=|\-\<|prec|\>\-|succ|\!in|sube|supe|\-\=|\~\=|cong|\~\~|prop|\=\>|_\|_|\(\:|\:\)|\<\<|\>\>|ceil|norm|uarr|darr|rarr|\-\>|larr|harr|rArr|lArr|hArr|ddot|sinh|cosh|tanh|sech|csch|coth|ast|div|o\+|o\.|sum|vee|vvv|cap|nnn|cup|uuu|int|del|O\/|\/_|sub|sup|and|not|neg|iff|bot|top|abs|hat|bar|vec|dot|sin|cos|tan|sec|csc|cot|exp|log|det|dim|mod|gcd|lcm|lub|glb|min|max|\+|\-|\*|xx|\@|ox|vv|nn|uu|pm|oo|CC|NN|QQ|RR|ZZ|\=|ne|\<|lt|\>|gt|le|ge|in|or|if|AA|EE|TT|\(|\)|\[|\]|\{|\}|to|ul|ln|f|g/,
+  },
+  {
+    token: "comment.block",
+    regex: /text\(.*?\)/,
+  },
+  {
+    token: "comment.block",
+    regex: /".*?"/,
+  },
+  {
+    token: "variable.other",
+    regex: /[a-zA-Z]/,
+  },
+  {
+    token: "constant.numeric",
+    regex: /[0-9]+(?:\.[0-9]+)?/,
+  },
+];
 
-define(
-  "ace/mode/asciimath_highlight_rules",
-  ["require", "exports", "ace/mode/text"],
-  (acequire: Acequire, exports: Exports): void => {
-  const TextHighlightRules = acequire("ace/mode/text_highlight_rules").TextHighlightRules;
+function defineMode<KS extends "start" | string>(name: string, highlightRules: HighlightRules<KS>) {
+  (ace as any).define(
+    `ace/mode/${name}`,
+    ["require", "exports", "ace/mode/text", `ace/mode/${name}_highlight_rules`],
+    (acequire: any, exports: any) => {
+      const TextMode = acequire("ace/mode/text").Mode;
+      const HighlightRules = acequire(`ace/mode/${name}_highlight_rules`).HighlightRules;
 
-  class AsciiMathHighlightRules extends TextHighlightRules {
-    $rules: { [key: string]: Array<{ token: string, regex: RegExp }> } = {
-      start: [
-        {
-          token: "keyword.operator",
-          regex: /color\(.*?\)/,
-        },
-        {
-          token: "variable.other",
-          regex: /varepsilon|vartheta|epsilon|upsilon|lambda|Lambda|varphi|alpha|gamma|Gamma|delta|Delta|theta|Theta|kappa|sigma|Sigma|omega|Omega|beta|zeta|iota|eta|rho|tau|phi|Phi|chi|psi|Psi|mu|nu|xi|Xi|pi|Pi/,
-        },
-        {
-          token: "keyword.operator",
-          regex: /\\\\|backslashsetminus|twoheadrightarrowtail|twoheadrightarrow|frac\{2\}\{3\}|\/_\\|triangle|rightarrowtail|leftrightarrow|Leftrightarrow|\.\.\.\||rightarrow|Rightarrow|underbrace|root|therefore|ldots|cdots|downarrow|leftarrow|Leftarrow|underline|overbrace|\|\>\<\||bidwedge|emptyset|\|\\\ \||\|quad\||lceiling|rceiling|subseteq|supseteq|\>\-\>\>|overline|underset|partial|because|diamond|implies|uparrow|overset|\*\*\*|\|\>\<|ltimes|\>\<\||rtimes|bowtie|otimes|\^\^\^|bigvee|bigcap|bigcup|square|lfloor|rfloor|\-\<\=|preceq|\>\-\=|succeq|subset|supset|approx|propto|\<\=\>|forall|exists|\|\-\-|\|\=\=|models|langle|rangle|\>\-\>|\-\>\>|\|\-\>|mapsto|ubrace|obrace|cancel|arcsin|arccos|arctan|times|oplus|wedge|nabla|infty|aleph|vdots|ddots|angle|frown|notin|equiv|vdash|floor|color|cdot|\*\*|star|\/\/|\-\:|circ|odot|prod|\^\^|2\/3|2\^3|sqrt|oint|grad|\+\-|\:\.|\:\'|\|__|__\||\|\~|\~\||\!\=|\<\=|\>\=|\-\<|prec|\>\-|succ|\!in|sube|supe|\-\=|\~\=|cong|\~\~|prop|\=\>|_\|_|\(\:|\:\)|\<\<|\>\>|ceil|norm|uarr|darr|rarr|\-\>|larr|harr|rArr|lArr|hArr|ddot|sinh|cosh|tanh|sech|csch|coth|ast|div|o\+|o\.|sum|vee|vvv|cap|nnn|cup|uuu|int|del|O\/|\/_|sub|sup|and|not|neg|iff|bot|top|abs|hat|bar|vec|dot|sin|cos|tan|sec|csc|cot|exp|log|det|dim|mod|gcd|lcm|lub|glb|min|max|\+|\-|\*|xx|\@|ox|vv|nn|uu|pm|oo|CC|NN|QQ|RR|ZZ|\=|ne|\<|lt|\>|gt|le|ge|in|or|if|AA|EE|TT|\(|\)|\[|\]|\{|\}|to|ul|ln|f|g/,
-        },
-        {
-          token: "comment.block",
-          regex: /text\(.*?\)/,
-        },
-        {
-          token: "comment.block",
-          regex: /".*?"/,
-        },
-        {
-          token: "variable.other",
-          regex: /[a-zA-Z]/,
-        },
-        {
-          token: "constant.numeric",
-          regex: /[0-9]+(?:\.[0-9]+)?/,
-        },
-      ],
-    };
-  }
+      exports.Mode = class extends TextMode {
+        HighlightRules: typeof HighlightRules = HighlightRules;
+      };
+    },
+  );
 
-  exports.AsciiMathHighlightRules = AsciiMathHighlightRules;
-});
+  (ace as any).define(
+    `ace/mode/${name}_highlight_rules`,
+    ["require", "exports", "ace/mode/text_highlight_rules"],
+    (acequire: any, exports: any): void => {
+      const TextHighlightRules = acequire("ace/mode/text_highlight_rules").TextHighlightRules;
+
+      exports.HighlightRules = class extends TextHighlightRules {
+        $rules: HighlightRules<KS> = highlightRules;
+      };
+    }
+  );
+}
+
+defineMode("asciimath", { start: asciimathState });
