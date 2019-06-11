2e06f66f544bb2977260c18dd703a2e9ec0e32dd
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-06-11 13:58:02 +0200

    tests(debouncer): test exception passing

--- tests/renderer.test.ts
+++ tests/renderer.test.ts
@@ -11,50 +11,52 @@ describe("Renderer", () => {
 
   (global as any).MathJax = { Hub: { Queue: jest.fn() } };
 
-  it("should render math by default", async () => {
-    const lineContainer = createLineContainer();
-    const renderer = new Renderer(lineContainer.id);
-    const math = "alpha + beta";
-    await renderer.render(math);
-    expect(lineContainer.innerHTML).toBe(`<div class="line">\`${math}\`</div>`);
-    expect(MathJax.Hub.Queue).toHaveBeenCalledWith(["Typeset", MathJax.Hub, lineContainer.firstChild]);
-  });
+  describe("#render()", () => {
+    it("renders math by default", async () => {
+      const lineContainer = createLineContainer();
+      const renderer = new Renderer(lineContainer.id);
+      const math = "alpha + beta";
+      await renderer.render(math);
+      expect(lineContainer.innerHTML).toBe(`<div class="line">\`${math}\`</div>`);
+      expect(MathJax.Hub.Queue).toHaveBeenCalledWith(["Typeset", MathJax.Hub, lineContainer.firstChild]);
+    });
 
-  it("should render markdown when prefixed with TEXT_PREFIX", async () => {
-    const lineContainer = createLineContainer();
-    const renderer = new Renderer(lineContainer.id);
-    await renderer.render(TEXT_PREFIX + "how **bold** of you");
-    expect(lineContainer.innerHTML).toBe(`<div class="line"><p>how <strong>bold</strong> of you</p>\n</div>`);
-  });
+    it("renders markdown when prefixed with TEXT_PREFIX", async () => {
+      const lineContainer = createLineContainer();
+      const renderer = new Renderer(lineContainer.id);
+      await renderer.render(TEXT_PREFIX + "how **bold** of you");
+      expect(lineContainer.innerHTML).toBe(`<div class="line"><p>how <strong>bold</strong> of you</p>\n</div>`);
+    });
 
-  it("should keep state consistent after concurrent render calls", async () => {
-    const lineContainer = createLineContainer();
-    const renderer = new Renderer(lineContainer.id);
+    it("keeps state consistent even after concurrent render calls", async () => {
+      const lineContainer = createLineContainer();
+      const renderer = new Renderer(lineContainer.id);
 
-    let toRender = "";
-    const actions = [
-      () => {
-        toRender = "";
-      },
-      () => {
-        toRender += Array(10)
-          .fill("a^2 + b^2 = c^2")
-          .join("\n\n");
-      },
-    ];
+      let toRender = "";
+      const actions = [
+        () => {
+          toRender = "";
+        },
+        () => {
+          toRender += Array(10)
+            .fill("a^2 + b^2 = c^2")
+            .join("\n\n");
+        },
+      ];
 
-    const promiseArray = [];
-    for (let i = 0; i < 10; i++) {
-      const act = actions[i % actions.length];
-      act();
-      promiseArray.push(renderer.render(toRender));
-    }
-    promiseArray.push(renderer.render(""));
+      const promiseArray = [];
+      for (let i = 0; i < 10; i++) {
+        const act = actions[i % actions.length];
+        act();
+        promiseArray.push(renderer.render(toRender));
+      }
+      promiseArray.push(renderer.render(""));
 
-    const results = await Promise.all(promiseArray);
-    expect(lineContainer.innerHTML).toBe('<div class="line">``</div>');
-    expect(results).toEqual(
-      new Array(promiseArray.length).fill(undefined).map((_, idx) => idx === promiseArray.length - 1),
-    );
+      const results = await Promise.all(promiseArray);
+      expect(lineContainer.innerHTML).toBe('<div class="line">``</div>');
+      expect(results).toEqual(
+        new Array(promiseArray.length).fill(undefined).map((_, idx) => idx === promiseArray.length - 1),
+      );
+    });
   });
 });
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -3,58 +3,71 @@ import { Debouncer } from "../js/utils";
 describe("Debouncer", () => {
   const sleep = (interval: number) => new Promise(resolve => setTimeout(resolve, interval));
 
-  it("should only call a function once if called many times", async () => {
-    const fn = jest.fn();
-    const debouncer = new Debouncer(10);
+  describe("#debounce()", () => {
+    it("only calls a function once if called many times", async () => {
+      const fn = jest.fn();
+      const debouncer = new Debouncer(10);
 
-    const debounced = [];
-    for (let i = 0; i < 100; ++i) {
-      debounced.push(debouncer.debounce(fn));
-    }
-    await Promise.all(debounced);
+      const debounced = [];
+      for (let i = 0; i < 100; ++i) {
+        debounced.push(debouncer.debounce(fn));
+      }
+      await Promise.all(debounced);
 
-    expect(fn).toHaveBeenCalledTimes(1);
-  });
-
-  it("should call a function many times if time passes", async () => {
-    const fn = jest.fn();
-    const debouncer = new Debouncer(10);
-
-    let counter = 0;
-
-    await new Promise(resolve => {
-      setTimeout(async function helper() {
-        await debouncer.debounce(fn);
-
-        if (++counter < 20) {
-          setTimeout(helper, debouncer.interval);
-        } else {
-          resolve();
-        }
-      }, debouncer.interval);
+      expect(fn).toHaveBeenCalledTimes(1);
     });
 
-    expect(fn).toHaveBeenCalledTimes(20);
-  });
+    it("calls a function many times if enough time passes", async () => {
+      const fn = jest.fn();
+      const debouncer = new Debouncer(10);
 
-  it("should resolve even if the function is not called", async () => {
-    const fn = jest.fn();
-    const debouncer = new Debouncer(10);
+      let counter = 0;
 
-    const notCalled = debouncer.debounce(fn);
-    const called = debouncer.debounce(fn);
+      await new Promise(resolve => {
+        setTimeout(async function helper() {
+          await debouncer.debounce(fn);
 
-    expect(await Promise.all([notCalled, called])).toEqual([false, true]);
-  });
+          if (++counter < 20) {
+            setTimeout(helper, debouncer.interval);
+          } else {
+            resolve();
+          }
+        }, debouncer.interval);
+      });
 
-  it("should await promises given to it", async () => {
-    let awaited = false;
-    const debouncer = new Debouncer(0);
-
-    await debouncer.debounce(async () => {
-      await sleep(100);
-      awaited = true;
+      expect(fn).toHaveBeenCalledTimes(20);
     });
-    expect(awaited).toBeTruthy();
+
+    it("resolves even if the function is not called", async () => {
+      const fn = jest.fn();
+      const debouncer = new Debouncer(10);
+
+      const notCalled = debouncer.debounce(fn);
+      const called = debouncer.debounce(fn);
+
+      expect(await Promise.all([notCalled, called])).toEqual([false, true]);
+    });
+
+    it("awaits promises given to it", async () => {
+      let awaited = false;
+      const debouncer = new Debouncer(0);
+
+      await debouncer.debounce(async () => {
+        await sleep(100);
+        awaited = true;
+      });
+      expect(awaited).toBeTruthy();
+    });
+
+    it("rejects when exceptions are thrown", async () => {
+      const up = new Error("yeet");
+      const debouncer = new Debouncer(0);
+
+      await expect(
+        debouncer.debounce(() => {
+          throw up;
+        }),
+      ).rejects.toThrow(up);
+    });
   });
 });
