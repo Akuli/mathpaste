d6ac0cafae0797bf22fa1cacaaf65c554388bc2a
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-06-21 20:55:37 +0200

    fix: actually fix alignment issues

--- js/highlight.ts
+++ js/highlight.ts
@@ -9,15 +9,23 @@ const createHighlighter = (rules: Rules) => {
   const rulesClasses = rules.map(({ cls }) => cls);
 
   return (s: string) =>
-    s
-      .replace(rulesRegex, (match, ...args) => {
-        // we skip the last two arguments because they are the offset and the whole string.
-        const groups = args.slice(0, -2) as Array<string | undefined>;
-        const groupIndex = groups.findIndex(g => g !== undefined);
-        const cls = rulesClasses[groupIndex];
-        return `<span class="${cls}">${match}</span>`;
-      })
-      .replace(/\n/g, "<br>");
+    s.replace(rulesRegex, (match, ...args) => {
+      // we skip the last two arguments because they are the offset and the whole string.
+      const groups = args.slice(0, -2) as Array<string | undefined>;
+      const groupIndex = groups.findIndex(g => g !== undefined);
+      const cls = rulesClasses[groupIndex];
+      return `<span class="${cls}">${match}</span>`;
+    });
+};
+
+// It's important we walk the whole tree before returning (i.e. create an
+// array, not a generator) because we can't mutate while iterating.
+const walk = (root: Node, whatToShow: number) => {
+  const walker = document.createTreeWalker(root, whatToShow);
+  const result = [];
+  let node;
+  while ((node = walker.nextNode()) !== null) result.push(node);
+  return result;
 };
 
 export default (editor: Editor, rules: Rules) => {
@@ -31,7 +39,14 @@ export default (editor: Editor, rules: Rules) => {
     overlay.style.width = (rect.width - getScrollbarWidth()).toString();
     overlay.style.height = rect.height.toString();
 
-    overlay.innerHTML = highlighter(editor.contents);
+    overlay.innerHTML = editor.element.innerHTML;
+
+    walk(overlay, NodeFilter.SHOW_TEXT).forEach(node => {
+      if (node.nodeValue === null) return;
+      const el = document.createElement("span");
+      el.innerHTML = highlighter(node.nodeValue);
+      overlay.replaceChild(el, node);
+    });
   };
 
   updateOverlay();
