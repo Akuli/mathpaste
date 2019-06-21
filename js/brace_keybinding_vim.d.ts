d593451c6e0596330ecacf00c73d65876db469e3
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-06-21 20:04:18 +0200

    chore: remove now-useless stuff

--- js/brace_keybinding_vim.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "brace/keybinding/vim" {}
--- js/modes/literate_asciimath.ts
+++ js/modes/literate_asciimath.ts
@@ -1,48 +1,45 @@
-import "brace/mode/markdown";
+// import { TEXT_PREFIX } from "../consts";
+// import "./asciimath";
 
-import { defineMode } from "./defineMode";
-import { TEXT_PREFIX } from "../consts";
-import "./asciimath";
+// defineMode(
+//   "literate_asciimath",
+//   {
+//     start: [
+//       {
+//         token: "comment.block",
+//         regex: "^" + TEXT_PREFIX,
+//         next: "markdown-start",
+//       },
+//       {
+//         token: "",
+//         regex: /(?=.)/,
+//         next: "asciimath-start",
+//       },
+//     ],
+//   },
+//   [
+//     {
+//       moduleName: "asciimath",
+//       highlightClassName: "HighlightRules",
+//       extraState: [
+//         {
+//           token: "",
+//           regex: /^$/,
+//           next: "start",
+//         },
+//       ],
+//     },
 
-defineMode(
-  "literate_asciimath",
-  {
-    start: [
-      {
-        token: "comment.block",
-        regex: "^" + TEXT_PREFIX,
-        next: "markdown-start",
-      },
-      {
-        token: "",
-        regex: /(?=.)/,
-        next: "asciimath-start",
-      },
-    ],
-  },
-  [
-    {
-      moduleName: "asciimath",
-      highlightClassName: "HighlightRules",
-      extraState: [
-        {
-          token: "",
-          regex: /^$/,
-          next: "start",
-        },
-      ],
-    },
-
-    {
-      moduleName: "markdown",
-      highlightClassName: "MarkdownHighlightRules",
-      extraState: [
-        {
-          token: "",
-          regex: /^$/,
-          next: "start",
-        },
-      ],
-    },
-  ],
-);
+//     {
+//       moduleName: "markdown",
+//       highlightClassName: "MarkdownHighlightRules",
+//       extraState: [
+//         {
+//           token: "",
+//           regex: /^$/,
+//           next: "start",
+//         },
+//       ],
+//     },
+//   ],
+// );
