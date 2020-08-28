bd6d67b312656a95c9c31171bd514660283d6fd4
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-08-28 18:02:45 +0200

    tests(canvasmanager): test undoAll()

---
+++ tests/canvasmanager.test.ts
@@ -0,0 +1,21 @@
+import { CanvasManager } from "../js/canvasmanager"
+import { Circle } from "../js/drawobjects/circle";
+
+describe("CanvasManager", () => {
+    it("can undo clearing the canvas", () => {
+        const canvas = document.createElement("canvas");
+        canvas.id = "canvas";
+        document.body.append(canvas);
+        const cm = new CanvasManager("canvas");
+
+        const circle = new Circle([0, 0], "red");
+
+        expect(cm.objects).toHaveLength(0);
+        cm.objects.push(circle);
+        expect(cm.objects).toEqual([circle]);
+        cm.undoAll();
+        expect(cm.objects).toHaveLength(0);
+        cm.undo();
+        expect(cm.objects).toEqual([circle]);
+    })
+})
