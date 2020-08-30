dd5d2ed83842735b4ff452056e62365e74be6db9
Akuli <akuviljanen17@gmail.com>
2020-08-30 14:37:37 +0300

    switch to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'double', {avoidEscape: true}],
+  }
+};
